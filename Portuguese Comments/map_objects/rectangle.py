89e8896f637679f2783f0c41a8e0244ee0ceb5e7
Anacronista <67645146+Anacronista@users.noreply.github.com>
2020-08-14 21:00:31 -0300

    Add files via upload

---
+++ Portuguese Comments/map_objects/rectangle.py
@@ -0,0 +1,17 @@
+#QUADRADOS TAMBÉM SÃO RETÂNGULOS, OK? #QUADRADOSLIVESMATTER
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return (center_x, center_y)
+
+    def intersect(self,other):
+        #resulta em verdadeiro caso esse retângulo intersecte com outro.
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+        self.y1 <= other.y2 and self.y2 >= other.y1)
